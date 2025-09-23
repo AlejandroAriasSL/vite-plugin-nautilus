216431fff477639d47608dae0990673dc116d19c
AlejandroAriasSL <alejandroasilva02@gmail>
2025-09-23 04:30:22 +0200

    chore: ignore .tgz files and allow debug flag on commands

--- scripts/run-command.js
+++ scripts/run-command.js
@@ -1,21 +1,30 @@
 import { spawn } from "child_process";
 import { resolve } from "path";
 
-export default async function runCommand(scriptPath){
+export default async function runCommand(scriptPath, inherit = false){
   const fullPath = resolve(process.cwd(), scriptPath);
+  console.log("el path completo del script",fullPath)
   return await new Promise((resolve, reject) => {
     const child = spawn("npx", ["tsx", fullPath], {
-      stdio: ["ignore", "pipe", "pipe"],
+      stdio: inherit ? "inherit" :  ["ignore", "pipe", "pipe"],
       shell: true,
     });
   
     let output = "";
-  
-    child.stdout.on("data", (data) => (output += data.toString()));
-    child.stderr.on("data", (data) => console.error(data.toString()));
+     if (!inherit && child.stdout && child.stderr) {
+      child.stdout.on("data", (data) => (output += data.toString()));
+      child.stderr.on("data", (data) => console.error(data.toString()));
+    }
   
     child.on("exit", (code) => {
-      if (code === 0) resolve(output.trim());
+      if (code === 0) {
+        if (!inherit){
+          resolve(output.trim());
+        } else {
+          resolve();
+        }
+
+      } 
       else reject(new Error("script failed to launch"));
     });
   });
